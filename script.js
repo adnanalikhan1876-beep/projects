03e97367bbc582f5f557c33c473a2ee99788a34a
Laraib Zahra <adnanalikhan1876@gmail.com>
2026-04-22 16:15:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("contactForm")?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for contacting Boutique Bliss! We'll get back to you soon.");
+});
